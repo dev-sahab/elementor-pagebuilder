277c090db46c7190e65f79488d8231324067eab5
shb-services <mdshahab.bc@gmail.com>
2022-06-07 23:08:50 +0600

    update

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const editorPanel = document.getElementById('editor-panel');
 const icon = document.querySelector('.icon')
 const eidtorBody = document.getElementById('eidtor-body');
 
+const emptySec = document.getElementById('empty-sec');
+
 switcher.onclick = () => {
     editorPanel.classList.toggle('remove');
     icon.classList.toggle('rotate');
@@ -11,16 +13,50 @@ switcher.onclick = () => {
 
 
 
-
 // get heading elements
 const headingInput =  document.getElementById('heading-input');
-const headingOutput =  document.getElementById('heading-output');
+// const headingOutput =  document.getElementById('heading-output');
 const hAlignment = document.querySelectorAll('#heading-editor #alignment input')
 const hfontWeight = document.querySelector('#heading-editor #font-weight')
 const hfontSize = document.querySelector('#heading-editor #font-size')
 const hColor = document.querySelector('#heading-editor #color');
 const hFontFamily = document.querySelector('#heading-editor #font-family');
+const heading = document.querySelector('.heading')
+const headingElement = document.getElementById('heading-element');
 
+const editorOptions = document.getElementById('editor-options');
+const toolLists = document.querySelectorAll('.tool-lists');
+
+
+editorOptions.style.display = 'none';
+
+// create custom h2 dom
+let headingOutput = '';
+headingElement.onclick = () => {
+
+    
+    headingOutput = document.createElement('h2');
+
+    const headingText = document.createTextNode('Add Your Heading Text Here');
+    headingOutput.append(headingText);
+    heading.appendChild(headingOutput);
+
+    headingOutput.style.fontSize = "30px"
+
+    toolLists.forEach( item => {
+        item.style.display = 'none';
+    })
+
+    editorOptions.style.display = 'block';
+    editorOptions.style.marginTop = '20px';
+
+    emptySec.style.display = 'none';
+
+}
+
+
+
+// if heading value will empty then show some text
 headingInput.onkeyup = () => {
     
     if(headingInput.value == ""){
@@ -32,6 +68,7 @@ headingInput.onkeyup = () => {
     
 }
 
+// heading Alignment
 hAlignment.forEach(item => {
     
     item.onchange = () => {
@@ -40,25 +77,25 @@ hAlignment.forEach(item => {
 
 });
 
+// Heading Font Weight
 hfontWeight.onchange = () => {
     headingOutput.style.fontWeight = hfontWeight.value;
 }
 
-headingOutput.style.fontSize = "30px"
-
-
+// Heading Font Size
 hfontSize.oninput = () => {
     headingOutput.style.fontSize = hfontSize.value + 'px';
 }
 
+// Heading color
 hColor.oninput = () => {
     headingOutput.style.color = hColor.value;
 }
 
+//Heading font-family
 hFontFamily.onchange = () => {
     headingOutput.style.fontFamily = hFontFamily.value;
 }
-
 
 
 
@@ -77,4 +114,19 @@ if (scrollbarVisible(eidtorBody) == true && isChrome == true
     eidtorBody.classList.remove('scroll');
 }
 
-console.log(isChrome);
+
+
+
+// if click on grid icon then tool list will show and editor options will hide
+const elementGrid = document.querySelector('.grid-icon')
+
+
+elementGrid.onclick = () => {
+
+    toolLists.forEach( item => {
+        item.style.display = 'grid';
+    });
+
+    editorOptions.style.display = 'none';
+    
+}
